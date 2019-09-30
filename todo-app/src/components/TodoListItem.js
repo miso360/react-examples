c69ec83fa69e1cf98bb3f4175203848a4872c875
Jun Park <dev.miso360@gmail.com>
2019-09-30 21:27:20 +0900

    Create Template

---
+++ todo-app/src/components/TodoListItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {
+    MdCheckBoxOutlineBlank,
+    MdCheckBox,
+    MdRemoveCircleOutline,
+} from 'react-icons/md';
+import './TodoListItem.scss';
+
+const TodoListItem = () => {
+    
+};
