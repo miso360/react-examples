9276b4f461413e1d5bbbd330726bf859af592c24
Jun Park <dev.miso360@gmail.com>
2019-10-01 22:57:01 +0900

    ...

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id:1,
+      text:'todo1',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: 'todo2',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: 'todo3',
+      checked: false,
+    }
+  ]);
+  
   return (
     <TodoTemplate>
       <TodoInsert/>
+      <TodoList todos={todos}/>
     </TodoTemplate>
   ); 
 };
