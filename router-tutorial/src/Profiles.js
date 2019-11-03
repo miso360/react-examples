aa65b2cf8bf1483c5ce90821a3f3af2f694d8b5d
Jun Park <dev.miso360@gmail.com>
2019-11-03 14:37:35 +0900

    NavLink

--- router-tutorial/src/Profiles.js
+++ router-tutorial/src/Profiles.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { Link, Route, NavLink } from 'react-router-dom';
 import Profile from './Profile';
 
 const Profiles = () => {
+    const activeStyle = {
+        background: 'black',
+        color: 'white'
+    };
     return (
         <div>
             <h3>사용자 목록:</h3>
             <ul>
                 <li>
-                    <Link to="/profiles/ironman">ironman</Link>
+                    {/* <Link to="/profiles/ironman">ironman</Link> */}
+                    <NavLink activeStyle={activeStyle} to="/profiles/ironman" active >
+                        ironman
+                    </NavLink>
                 </li>
                 <li>
-                    <Link to="/profiles/captainamerica">captainamerica</Link>
+                    <NavLink activeStyle={activeStyle} to="/profiles/captainamerica" active >
+                        captainamerica
+                    </NavLink>
                 </li>
             </ul>
 
