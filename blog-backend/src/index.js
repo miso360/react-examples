42c51ffa970b4263d502bcc436550f0ffa317b10
Jun Park <dev.miso360@gmail.com>
2020-01-25 04:45:46 +0900

    router sample

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -9,8 +9,15 @@ router.get('/', ctx => {
    ctx.body = 'Home';
 });
 
-router.get('/about', ctx => {
-   ctx.body = 'About';
+router.get('/about/:name?', ctx => {
+   const { name } = ctx.params;
+   // name의 존재 유무에 따라 다른 결과 출력
+   ctx.body = name ? `${name}의 소개` : '소개';
+});
+
+router.get('/posts', ctx => {
+   const { id } = ctx.query;
+   ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
 });
 
 // app 인스턴스에 라우터 적용
