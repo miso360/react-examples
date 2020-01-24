9c7402e06306b3c6eea4091e9f5bfedb9a2d0bd7
Jun Park <dev.miso360@gmail.com>
2020-01-25 00:53:00 +0900

    router

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,1 +1,21 @@
-const hello = "hello";
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+
+// 라우터 설정
+router.get('/', ctx => {
+   ctx.body = 'Home';
+});
+
+router.get('/about', ctx => {
+   ctx.body = 'About';
+});
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(4000, () => {
+   console.log('Listening to port 4000');
+});
