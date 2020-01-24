1f0078875954af981811c3f07606a7083e1e0486
Jun Park <dev.miso360@gmail.com>
2020-01-24 14:38:11 +0900

    start blog-backend

--- blog-frontend/src/containers/auth/RegisterForm.js
+++ blog-frontend/src/containers/auth/RegisterForm.js
@@ -2,13 +2,16 @@ import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {changeField, initializeForm, register} from "../../modules/auth";
 import AuthForm from '../../components/auth/AuthForm';
+import { check } from '../../modules/user';
+// import { withRouter } from 'react-router-dom';
 
-const RegisterForm = () => {
+const RegisterForm = (history) => {
     const dispatch = useDispatch();
-    const {form, auth, authError} = useSelector(({auth}) => ({
+    const {form, auth, authError, user} = useSelector(({auth, user}) => ({
         form: auth.register,
         auth: auth.auth,
-        authError: auth.authError
+        authError: auth.authError,
+        user: user.user
     }));
 
     // 인풋 변경 이벤트 핸들러
@@ -53,6 +56,14 @@ const RegisterForm = () => {
         }
     }, [auth, authError]);
 
+    // user 값이 잘 설정되었는지 확인
+    useEffect(() => {
+        if (user) {
+            console.log('check API 성공');
+            console.log(user);
+        }
+    }, [user]);
+
     return (
         <AuthForm
             type="register"
@@ -64,3 +75,4 @@ const RegisterForm = () => {
 };
 
 export default RegisterForm;
+// export default withRouter(RegisterForm);
