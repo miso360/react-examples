664bcaab17a1b59969e3c12fcbfabc9fe2849d3a
solnote <dev.miso360@gmail.com>
2020-02-01 16:59:31 +0900

    posts ctrl

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -13,10 +13,103 @@ const posts = [
 POST /api/posts
 { title, body }
  */
-export.write = ctx => {
+exports.write = ctx => {
     // REST API의 Request Body는 ctx.request.body에서 조회할 수 있습니다.
-    const { title, body } = ctx.request.body;
+    const {title, body} = ctx.request.body;
     postId += 1;
-    const post = { id: postId, title, body};
+    const post = {id: postId, title, body};
     posts.push(post);
+};
+
+/* 포스트 목록 조회
+GET /api/posts
+ */
+exports.list = ctx => {
+    ctx.body = posts;
+};
+
+/* 특정 포스트 조회
+GET /api/posts/:id
+ */
+exports.read = ctx => {
+    const {id} = ctx.params;
+    // 파라미터로 받아 온 값은 문자열 형식이므로 파라미터를 숫자로 변환
+    // id로 포스트를 찾는다.
+    const post = posts.find(p => p.id.toString() === id);
+
+    if (!post) {
+        ctx.status = 404;
+        ctx.body = {
+            message: '포스트가 존재하지 않습니다.',
+        };
+        return;
+    }
+    ctx.body = post;
+}
+
+/* 특정 포스트 제거
+DELETE /api/posts/:id
+ */
+exports.remove = ctx => {
+    const {id} = ctx.params;
+    const index = posts.findIndex(p => p.id.toString() === id);
+    if (index === -1) {
+        ctx.status = 404;
+        ctx.body = {
+            message: '포스트가 존재하지 않습니다.',
+        };
+        return;
+    }
+
+    // index번째 아이템 제거
+    posts.splice(index, 1);
+    ctx.status = 204; // No content
+};
+
+/* 포스트 수정(교체)
+PUT /api/posts/:id
+{ title, body }
+ */
+exports.replace = ctx => {
+    // PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할 때 사용
+    const {id} = ctx.params;
+    const index = posts.findIndex(p => p.id.toString() === id);
+
+    if (index === -1) {
+        ctx.status = 404;
+        ctx.body = {
+            message: '포스트가 존재하지 않습니다.',
+        };
+        return;
+    }
+
+    // id를 제외한 정보 전체 객체를 덮어 씌움
+    posts[index] = {
+        id,
+        ...ctx.request.body,
+    };
+    ctx.body = posts[index];
+}
+
+/* 포스트 수정(특정 필드 변경)
+PATCH /api/posts/:id
+{ title, body }
+ */
+exports.update = ctx => {
+    // PATCH 메서드는 주어진 필드만 교체
+    const {id} = ctx.params;
+    const index = posts.findIndex(p => p.id.toString() === id);
+    if (index === -1) {
+        ctx.status = 404;
+        ctx.body = {
+            message: '포스트가 존재하지 않습니다.',
+        };
+        return;
+    }
+    // 기존 값에 정보를 덮어 씌운다.
+    posts[index] = {
+        ...posts[index],
+        ...ctx.request.body,
+    };
+    ctx.body = posts[index];
 };
