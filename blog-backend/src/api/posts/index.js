ba4a3f5c0d417e39fce011d7beb66c183cdf3875
solnote <dev.miso360@gmail.com>
2020-02-02 15:09:59 +0900

    posts ctrl

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -12,10 +12,10 @@ const posts = new Router();
 // };
 
 posts.get('/', postsCtrl.list);
-posts.post('/', postsCtrl.write());
+posts.post('/', postsCtrl.write);
 posts.get('/:id', postsCtrl.read);
-posts.delete('/id', postsCtrl.remove());
-posts.put('/:id', postsCtrl.replace());
-posts.patch('/:id', postsCtrl.update())
+posts.delete('/:id', postsCtrl.remove);
+posts.put('/:id', postsCtrl.replace);
+posts.patch('/:id', postsCtrl.update);
 
 module.exports = posts;
