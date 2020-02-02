75b944b559b5894fed7ea77d6ea82c4bbde83471
solnote <dev.miso360@gmail.com>
2020-02-02 13:55:57 +0900

    posts ctrl - my

--- blog-backend/src/api/index.js
+++ blog-backend/src/api/index.js
@@ -1,5 +1,6 @@
 const Router = require('koa-router');
 const posts = require('./posts');
+
 const api = new Router();
 
 api.use('/posts', posts.routes());
--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -15,10 +15,11 @@ POST /api/posts
  */
 exports.write = ctx => {
     // REST API의 Request Body는 ctx.request.body에서 조회할 수 있습니다.
-    const {title, body} = ctx.request.body;
+    const { title, body } = ctx.request.body;
     postId += 1;
     const post = {id: postId, title, body};
     posts.push(post);
+    ctx.body = post;
 };
 
 /* 포스트 목록 조회
