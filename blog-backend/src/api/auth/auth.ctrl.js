e1371cd491e68ddb79a2749edb400e4df3fbfa4a
박준 <solsol@douzone.com>
2020-02-08 23:21:14 +0900

    Create api for register user

--- blog-backend/src/api/auth/auth.ctrl.js
+++ blog-backend/src/api/auth/auth.ctrl.js
@@ -1,5 +1,50 @@
+import Joi from 'joi';
+import User from '../../models/user';
+
+/*
+    POST /api/auth/register
+    {
+        username: 'solsol',
+        password: '111111'
+    }
+ */
 export const register = async ctx => {
     // 회원가입
+    // Request Body 검증
+    const schema = Joi.object().keys({
+        username: Joi.string()
+            .alphanum()
+            .min(3)
+            .max(20)
+            .required(),
+        password: Joi.string().required(),
+    });
+    const result = Joi.validate(ctx.request.body, schema);
+    if (result.error) {
+        ctx.status = 400;
+        ctx.body = result.error;
+        return;
+    }
+
+    const {username, password} = ctx.request.body;
+    try {
+        // username이 이미 존재하는지 확인
+        const exists = await User.findByUsername(username);
+        if (exists) {
+            ctx.status = 409;
+            return;
+        }
+
+        const user = new User({
+           username,
+        });
+        await user.setPassword(password);
+        await user.save();
+        ctx.body = user.serialize();
+
+    } catch (e) {
+        ctx.throw(500, e);
+    }
 };
 
 export const login = async ctx => {
